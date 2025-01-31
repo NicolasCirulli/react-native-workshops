a241f3ad3206cd79b722daa53b2d4bc3a404cf6e
nicolascirulli <nicolascirulli2@gmail.com>
2025-01-31 16:44:27 -0300

    workshop 31-01-25

---
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import ScreenWrapper from "@/core/components/wrappers/ScreenWrapper";
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <ScreenWrapper>
+      <Text>Home Screen</Text>
+    </ScreenWrapper>
+  );
+}
