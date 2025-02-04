45f66dc86da22c8999839f0dd5917c3d889d87ac
nicolascirulli <nicolascirulli2@gmail.com>
2025-02-04 17:10:52 -0300

    feat: Ampliada la personalización con soporte para className y control de marginTop

--- src/core/components/wrappers/ScreenWrapper.tsx
+++ src/core/components/wrappers/ScreenWrapper.tsx
@@ -2,22 +2,22 @@ import { View, ViewProps } from "react-native";
 import React from "react";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-interface Props extends ViewProps {}
+interface Props extends ViewProps {
+  mt?: boolean; // Agrego una prop que no pertenece a las de ViewProps
+}
 
-export default function ScreenWrapper({ style, ...rest }: Props) {
+export default function ScreenWrapper({
+  mt = false, // La prop es opcional y por defecto la dejo en false
+  className,
+  style,
+  ...rest
+}: Props) {
   const inset = useSafeAreaInsets();
 
   return (
     <View
-      style={[
-        {
-          flex: 1,
-          marginTop: inset.top,
-          justifyContent: "center",
-          alignItems: "center",
-        },
-        style,
-      ]}
+      style={[{ marginTop: mt ? inset.top : 0 }, style]}
+      className={"flex-1 " + className}
       {...rest}
     />
   );
